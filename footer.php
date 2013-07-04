936fb6c7a9b4f97ba1406c10b1c7a4a939277575
binhdo <m@binaryhideout.com>
2013-07-04 16:41:35 +0200

    move in rebranded twentythirteen

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Binary_Foundation
+ * @since Binary Foundation 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				<?php do_action( 'binaryfoundation_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'binaryfoundation' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'binaryfoundation' ); ?>"><?php printf( __( 'Proudly powered by %s', 'binaryfoundation' ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ inc/back-compat.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Binary Foundation back compat functionality.
+ *
+ * Prevents Binary Foundation from running on WordPress versions prior to 3.6,
+ * since this theme is not meant to be backwards compatible and relies on
+ * many new functions and markup changes introduced in 3.6.
+ *
+ * @package WordPress
+ * @subpackage Binary_Foundation
+ * @since Binary Foundation 1.0
+ */
+
+/**
+ * Prevent switching to Binary Foundation on old versions of WordPress. Switches
+ * to the default theme.
+ *
+ * @since Binary Foundation 1.0
+ *
+ * @return void
+ */
+function binaryfoundation_switch_theme() {
+	switch_theme( WP_DEFAULT_THEME, WP_DEFAULT_THEME );
+	unset( $_GET['activated'] );
+	add_action( 'admin_notices', 'binaryfoundation_upgrade_notice' );
+}
+add_action( 'after_switch_theme', 'binaryfoundation_switch_theme' );
+
+/**
+ * Prints an update nag after an unsuccessful attempt to switch to
+ * Binary Foundation on WordPress versions prior to 3.6.
+ *
+ * @since Binary Foundation 1.0
+ *
+ * @return void
+ */
+function binaryfoundation_upgrade_notice() {
+	$message = sprintf( __( 'Binary Foundation requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'binaryfoundation' ), $GLOBALS['wp_version'] );
+	printf( '<div class="error"><p>%s</p></div>', $message );
+}
+
+/**
+ * Prevents the Customizer from being loaded on WordPress versions prior to 3.6.
+ *
+ * @since Binary Foundation 1.0
+ *
+ * @return void
+ */
+function binaryfoundation_customize() {
+	wp_die( sprintf( __( 'Binary Foundation requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'binaryfoundation' ), $GLOBALS['wp_version'] ), '', array(
+		'back_link' => true,
+	) );
+}
+add_action( 'load-customize.php', 'binaryfoundation_customize' );
+
+/**
+ * Prevents the Theme Preview from being loaded on WordPress versions prior to 3.4.
+ *
+ * @since Binary Foundation 1.0
+ *
+ * @return void
+ */
+function binaryfoundation_preview() {
+	if ( isset( $_GET['preview'] ) ) {
+		wp_die( sprintf( __( 'Binary Foundation requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'binaryfoundation' ), $GLOBALS['wp_version'] ) );
+	}
+}
+add_action( 'template_redirect', 'binaryfoundation_preview' );
