92517201ae2506333e687d360e773eb4037e457d
binhdo <m@binaryhideout.com>
2013-07-30 15:00:30 +0200

    update to Foundation 4.3.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,20 +23,21 @@ module.exports = function(grunt) {
 				files: {
 					'js/foundation.min.js': [
 						'js/foundation/foundation.js',
-						'js/foundation/foundation.dropdown.js',
-						'js/foundation/foundation.reveal.js',
-						//'js/foundation/foundation.joyride.js',
-						//'js/foundation/foundation.interchange.js',
 						'js/foundation/foundation.forms.js',
-						'js/foundation/foundation.placeholder.js',
-						'js/foundation/foundation.tooltips.js',
-						'js/foundation/foundation.clearing.js',
-						'js/foundation/foundation.cookie.js',
-						'js/foundation/foundation.orbit.js',
 						'js/foundation/foundation.section.js',
+						'js/foundation/foundation.reveal.js',
+						'js/foundation/foundation.clearing.js',
+						//'js/foundation/foundation.interchange.js',
+						'js/foundation/foundation.tooltips.js',
+						'js/foundation/foundation.cookie.js',
+						//'js/foundation/foundation.joyride.js',
+						'js/foundation/foundation.alerts.js',
+						'js/foundation/foundation.abide.js',
 						'js/foundation/foundation.magellan.js',
+						'js/foundation/foundation.orbit.js',
 						'js/foundation/foundation.topbar.js',
-						'js/foundation/foundation.alerts.js'
+						'js/foundation/foundation.dropdown.js',
+						'js/foundation/foundation.placeholder.js'
 					]
 				}
 			},
